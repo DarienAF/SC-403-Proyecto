fc6b57c8a0d3928535567aa5b5175ba61f435f8c
DarienAF <jafdarien@gmail.com>
2023-10-30 18:08:24 -0600

    Push 1 Darien

---
+++ src/main/java/com/sc403/Sc403Application.java
@@ -0,0 +1,13 @@
+package com.sc403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sc403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc403/Sc403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sc403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
