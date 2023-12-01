1bff43dded1028d71b411547f8bbb2f328105a81
DarienAF <jafdarien@gmail.com>
2023-12-01 15:05:09 -0600

    Base de datos actualizada

---
+++ src/main/java/com/sc403/controller/VistaController.java
@@ -0,0 +1,51 @@
+
+package com.sc403.controller;
+
+import com.sc403.domain.Categoria;
+import com.sc403.domain.Producto;
+import com.sc403.service.CategoriaService;
+import com.sc403.service.ProductoService;
+import com.sc403.service.FirebaseStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/vistas")
+public class VistaController {
+    @Autowired
+    private ProductoService productoService;
+    @Autowired
+    private CategoriaService categoriaService;
+    
+    @GetMapping("/vista")
+    public String listado(Model model) {
+        var productos = productoService.getProductos(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        
+        return "/vistas/vista";
+    }
+    
+    @GetMapping("/vista/{idCategoria}")
+    public String listado(Model model, Categoria categoria) {
+        var catego = categoriaService.getCategoria(categoria);
+        var productos = catego.getProductos();
+                
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        
+        return "/vistas/vista";
+    }
+}
